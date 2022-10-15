2d302c2c92cc9bc6b73b83378e4743fba737fa9b
TJ202 <72973488+TJ202@users.noreply.github.com>
2022-10-16 02:04:45 +0530

    add: stego en-dec

--- code.py
+++ code.py
@@ -2,8 +2,13 @@ from Crypto.Cipher import AES
 from Crypto.Cipher import DES3
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
+import sys
+import codecs
+import numpy as np
+from PIL import Image
+np.set_printoptions(threshold=sys.maxsize)
 
-#Global variables for Layer 1
+# Global variables for Layer 1
 public_key = RSA.import_key(open("keys/layer1Public.pem").read())
 private_key = RSA.import_key(open("keys/layer1Private.pem").read())
 cipher_e = PKCS1_OAEP.new(public_key)
@@ -20,6 +25,10 @@ with open('keys/layer2.pem', 'rb') as p:
     key3 = p.read()
 E_cipher = DES3.new(key3, DES3.MODE_EAX)
 D_cipher = DES3.new(key3, DES3.MODE_EAX, E_cipher.nonce)
+
+# For steganography
+src = "input.png"
+dest = "output.png"
 
 def main():
     with open('plaintext.txt', 'rb') as p:
@@ -47,7 +56,80 @@ def layer3(plaintext):
     # Generating Ciphertext
     ciphertext = E_cipher.encrypt(plaintext)
     # Sending Ciphertext to Layer 3 for Decryption
+    # layer3_D(ciphertext)
+    print(ciphertext)
+    StegEncode(src,ciphertext, dest)
+
+#encoding function
+def StegEncode(src, message, dest):
+
+    img = Image.open(src, 'r')
+    width, height = img.size
+    array = np.array(list(img.getdata()))
+
+    if img.mode == 'RGB':
+        n = 3
+    elif img.mode == 'RGBA':
+        n = 4
+
+    total_pixels = array.size//n
+
+    print(type(message))
+    message = codecs.decode(message)
+    message += "$t3g0"
+    b_message = ''.join([format(ord(i), "08b") for i in message])
+    req_pixels = len(b_message)
+
+    if req_pixels > total_pixels:
+        print("ERROR: Need larger file size")
+
+    else:
+        index=0
+        for p in range(total_pixels):
+            for q in range(0, 3):
+                if index < req_pixels:
+                    array[p][q] = int(bin(array[p][q])[2:9] + b_message[index], 2)
+                    index += 1
+
+        array=array.reshape(height, width, n)
+        enc_img = Image.fromarray(array.astype('uint8'), img.mode)
+        enc_img.save(dest, 'png')
+        print("Image StegEncoded Successfully")
+
+
+#decoding function
+def StegDecode(src):
+
+    img = Image.open(src, 'r')
+    array = np.array(list(img.getdata()))
+
+    if img.mode == 'RGB':
+        n = 3
+    elif img.mode == 'RGBA':
+        n = 4
+
+    total_pixels = array.size//n
+
+    hidden_bits = ""
+    for p in range(total_pixels):
+        for q in range(0, 3):
+            hidden_bits += (bin(array[p][q])[2:][-1])
+
+    hidden_bits = [hidden_bits[i:i+8] for i in range(0, len(hidden_bits), 8)]
+
+    message = ""
+    for i in range(len(hidden_bits)):
+        if message[-5:] == "$t3g0":
+            break
+        else:
+            message += chr(int(hidden_bits[i], 2))
+    if "$t3g0" in message:
+        print("Hidden Message:", message[:-5])
+    else:
+        print("No Hidden Message Found")
+    ciphertext = message[:-5].encode('utf-8')
     layer3_D(ciphertext)
+
 
 # Layer 3 - Decryption using DES3
 def layer3_D(ciphertext):
