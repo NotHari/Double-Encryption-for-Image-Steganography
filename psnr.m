f8aef42629d9a4e68ca7d73d42fd4159ec4ab137
TJ202 <72973488+TJ202@users.noreply.github.com>
2023-03-05 22:06:38 +0530

    updated: psnr.m

--- psnr.m
+++ psnr.m
@@ -1,19 +1,16 @@
-A = imread("C:\Users\hariv\OneDrive\Desktop\Things\fall-semester\CSE4003 - Cyber Security\project\review-2\Double-Encryption\images\coverImage1.png");
-ref = imread("C:\Users\hariv\OneDrive\Desktop\Things\fall-semester\CSE4003 - Cyber Security\project\review-2\Double-Encryption\images\coverImageSecret1.png");
-[peaksnr,snr] = psnr(A, ref); 
+A = imread("C:\projects\Double-Encryption-for-Image-Steganography\images\tulips.png");
+ref1 = imread("C:\projects\Double-Encryption-for-Image-Steganography\images\tulipsSecret1.png");
+ref2 = imread("C:\projects\Double-Encryption-for-Image-Steganography\images\tulipsSecret2.png");
+ref3 = imread("C:\projects\Double-Encryption-for-Image-Steganography\images\tulipsSecret3.png");
+ref4 = imread("C:\projects\Double-Encryption-for-Image-Steganography\images\tulipsSecret4.png");
+ref5 = imread("C:\projects\Double-Encryption-for-Image-Steganography\images\tulipsSecret5.png");
 
-disp(peaksnr);
+[peaksnr1,snr1] = psnr(A, ref1); 
+[peaksnr2,snr2] = psnr(A, ref2); 
+[peaksnr3,snr3] = psnr(A, ref3); 
+[peaksnr4,snr4] = psnr(A, ref4); 
+[peaksnr5,snr5] = psnr(A, ref5);
 
-% A = imread("C:\Users\hariv\OneDrive\Desktop\Things\fall-semester\CSE4003 - Cyber Security\project\review-2\Double-Encryption\images\coverImage2.png");
-% ref = imread("C:\Users\hariv\OneDrive\Desktop\Things\fall-semester\CSE4003 - Cyber Security\project\review-2\Double-Encryption\images\coverImageSecret2.png");
-% [peaksnr,snr] = psnr(A, ref); 
-
-% disp(peaksnr);
-
-% A = imread("C:\Users\hariv\OneDrive\Desktop\Things\fall-semester\CSE4003 - Cyber Security\project\review-2\Double-Encryption\images\coverImage3.png");
-% ref = imread("C:\Users\hariv\OneDrive\Desktop\Things\fall-semester\CSE4003 - Cyber Security\project\review-2\Double-Encryption\images\coverImageSecret3.png");
-% [peaksnr,snr] = psnr(A, ref); 
-
-% disp(peaksnr);
-
-
+disp("PSNR: "+peaksnr1+" "+peaksnr2+" "+peaksnr3+" "+peaksnr4+" "+peaksnr5);
+disp("SSIM: "+ssim(A, ref1)+" "+ssim(A, ref2)+" "+ssim(A, ref3)+" "+ssim(A, ref4)+" "+ssim(A, ref5));
+disp("MSE: "+immse(A, ref1)+" "+immse(A, ref2)+" "+immse(A, ref3)+" "+immse(A, ref4)+" "+immse(A, ref5));
