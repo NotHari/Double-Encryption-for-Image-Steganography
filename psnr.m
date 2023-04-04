ab8dda6773ed80e124af64d81e98b944fd5fa734
NotHari <80103986+NotHari@users.noreply.github.com>
2023-04-05 03:37:31 +0530

    fix: rsa length

--- psnr.m
+++ psnr.m
@@ -1,9 +1,9 @@
-A = imread("C:\projects\Double-Encryption-for-Image-Steganography\images\tulips.png");
-ref1 = imread("C:\projects\Double-Encryption-for-Image-Steganography\images\tulipsSecret1.png");
-ref2 = imread("C:\projects\Double-Encryption-for-Image-Steganography\images\tulipsSecret2.png");
-ref3 = imread("C:\projects\Double-Encryption-for-Image-Steganography\images\tulipsSecret3.png");
-ref4 = imread("C:\projects\Double-Encryption-for-Image-Steganography\images\tulipsSecret4.png");
-ref5 = imread("C:\projects\Double-Encryption-for-Image-Steganography\images\tulipsSecret5.png");
+A = imread("images\tulips.png");
+ref1 = imread("images\tulipsSecret1.png");
+ref2 = imread("images\tulipsSecret2.png");
+ref3 = imread("images\tulipsSecret3.png");
+ref4 = imread("images\tulipsSecret4.png");
+ref5 = imread("images\tulipsSecret5.png");
 
 [peaksnr1,snr1] = psnr(A, ref1); 
 [peaksnr2,snr2] = psnr(A, ref2); 
@@ -14,3 +14,4 @@ ref5 = imread("C:\projects\Double-Encryption-for-Image-Steganography\images\tuli
 disp("PSNR: "+peaksnr1+" "+peaksnr2+" "+peaksnr3+" "+peaksnr4+" "+peaksnr5);
 disp("SSIM: "+ssim(A, ref1)+" "+ssim(A, ref2)+" "+ssim(A, ref3)+" "+ssim(A, ref4)+" "+ssim(A, ref5));
 disp("MSE: "+immse(A, ref1)+" "+immse(A, ref2)+" "+immse(A, ref3)+" "+immse(A, ref4)+" "+immse(A, ref5));
+
